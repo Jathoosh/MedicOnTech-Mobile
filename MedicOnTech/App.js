b394d83ff2c1e642996f11f2d1b4750715f2989e
Maxence Robi <maxence.robichon@efrei.net>
2022-06-13 12:12:22 +0200

    V 1.3 (ajout des polices)

--- MedicOnTech/App.js
+++ MedicOnTech/App.js
@@ -1,8 +1,18 @@
 import { StyleSheet } from "react-native";
 
-import PageConnexion from "./screens/pageConnexion";
+import PageConnexion from "./screens/PageConnexion";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "cera-pro-light": require("./assets/cera pro font/Cera Pro Light.ttf"),
+    "cera-pro-black-italic": require("./assets/cera pro font/Cera Pro Black Italic.ttf"),
+    "cera-pro-regular-italic": require("./assets/cera pro font/Cera Pro Regular Italic.ttf"),
+    "cera-pro-medium": require("./assets/cera pro font/Cera Pro Medium.ttf"),
+    "cera-pro-bold": require("./assets/cera pro font/Cera Pro Bold.ttf"),
+    "cera-pro-black": require("./assets/cera pro font/Cera Pro Black.ttf"),
+  });
+
   return <PageConnexion />;
 }
 
