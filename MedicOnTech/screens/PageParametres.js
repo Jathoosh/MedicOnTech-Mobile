d40e0a3cc8c0173547a75c4023e2693512523b61
Gyoomu <60664665+Gyoomu@users.noreply.github.com>
2022-07-05 16:46:52 +0200

    style dropdown

--- MedicOnTech/screens/PageParametres.js
+++ MedicOnTech/screens/PageParametres.js
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Text, Pressable } from "react-native";
+import { View, StyleSheet, Text, Pressable, Switch } from "react-native";
 import SelectDropdown from 'react-native-select-dropdown'
 const languages = ["Français", "English"];
 const themes = ["sombre", "clair"];
@@ -19,11 +19,20 @@ function PageParametres() {
         <View style={styles.containerButtons}>
               <View style={styles.button}>
                   <Text style={styles.buttonText}>Thème</Text>
+                  <View style={styles.dropList}>
                   <SelectDropdown
                     data={themes}
                     onSelect={(selectedItem, index) => {
                       console.log(selectedItem, index)
                     }}
+                    renderDropdownIcon={() => (
+                      <View style={styles.dropdownIcon}>
+                        <Text style={styles.dropdownButtonText}>▼</Text>
+                      </View>
+                    )}
+                    buttonStyle={styles.dropdownButton}
+                    buttonTextStyle={styles.dropdownButtonText}
+                    defaultButtonText={themes[0]}
                     buttonTextAfterSelection={(selectedItem, index) => {
                       // text represented after item is selected
                       // if data array is an array of objects then return selectedItem.property to render after item is selected
@@ -35,15 +44,25 @@ function PageParametres() {
                       return item
                     }}
                   />
+                  </View>
               </View>
             
               <View style={styles.button}>
                   <Text style={styles.buttonText}>Langue</Text>
+                  <View style={styles.dropList}>
                   <SelectDropdown
                     data={languages}
                     onSelect={(selectedItem, index) => {
                       console.log(selectedItem, index)
                     }}
+                    renderDropdownIcon={() => (
+                      <View style={styles.dropdownIcon}>
+                        <Text style={styles.dropdownButtonText}>▼</Text>
+                      </View>
+                    )}
+                    buttonStyle={styles.dropdownButton}
+                    buttonTextStyle={styles.dropdownButtonText}
+                    defaultButtonText={languages[0]}
                     buttonTextAfterSelection={(selectedItem, index) => {
                       // text represented after item is selected
                       // if data array is an array of objects then return selectedItem.property to render after item is selected
@@ -55,6 +74,7 @@ function PageParametres() {
                       return item
                     }}
                   />
+                  </View>
               </View>
             
             
@@ -76,6 +96,7 @@ const styles = StyleSheet.create({
     marginTop: heightPixel(20),
     borderTopWidth: 1,
     borderTopColor: "#E0E0E0",
+    
   },
   
   button: {
@@ -84,4 +105,33 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: "#E0E0E0",
   },
+  buttonText: {
+    fontFamily: "cera-pro-medium",
+    fontSize: fontPixel(18),
+    marginLeft: widthPixel(20),
+    alignSelf: "center",
+  },
+  dropList: {
+    marginLeft: "auto",
+    marginRight: 0,
+  },
+  dropdownIcon: {
+    display: "flex",
+    flexDirection: "row",
+    alignSelf: "center",
+    marginRight: widthPixel(10),
+  },
+  
+  dropdownButton: {
+    backgroundColor: "#fff",
+    width: widthPixel(300),
+  },
+  dropdownButtonText: {
+    fontFamily: "cera-pro-medium",
+    fontSize: fontPixel(18),
+    alignSelf: "center",
+    display: "flex",
+    alignItems: "flex-end",
+  },
+
 });
